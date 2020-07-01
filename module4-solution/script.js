68b530d0e94b219767923f34f4e066d3b721d46a
sayali1711 <66635685+sayali1711@users.noreply.github.com>
2020-07-01 15:30:24 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,4 +1,5 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i=0; i<10; i++) { 
 var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
@@ -9,3 +10,4 @@ var firstLetter = names[i].charAt(0).toLowerCase();
   }
     
 }
+})();
